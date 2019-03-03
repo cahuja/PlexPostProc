caaad2516409ea789c5113e8e9aba03c448e250c
Chetan Ahuja <ahuja.chetan@gmail.com>
2019-03-03 12:57:16 -0500

    Subtitle processing

--- PlexPostProc.sh
+++ PlexPostProc.sh
@@ -58,19 +58,25 @@ if [ ! -z "$1" ]; then
    #MYPID=$$	# Process ID for current script
    # Adjust niceness of CPU priority for the current process
    #renice 19 $MYPID
+   echo "********************************************************"
+   echo "Extracting subtitles w/ ccextractor for $FILENAME"
+   echo "********************************************************"
+   TEMPSRTFILENAME="/tmp/$(basename "$FILENAME" .ts).srt"
+   ccextractor "$FILENAME" -o "$TEMPSRTFILENAME"  || fatal "ccextractor has failed. Is it installed? There should be a symbolic link in /usr/local/bin"
 
    echo "********************************************************"
    echo "Transcoding, Converting to H.264 w/Handbrake"
    echo "********************************************************"
-   HandBrakeCLI -i "$FILENAME" -f mkv --aencoder copy -e qsv_h264 --x264-preset veryfast --x264-profile auto -q 16 --maxHeight 720 --decomb bob -o "$TEMPFILENAME" || fatal "Handbreak has failed (Is it installed?)"
+   HandBrakeCLI -i "$FILENAME" -f mkv --srt-file "$TEMPSRTFILENAME" -srt-lang eng --srt-default --aencoder copy -e qsv_h264 --x264-preset veryfast --x264-profile auto -q 16 --maxHeight 1080 --decomb bob -o "$TEMPFILENAME" || fatal "Handbreak has failed (Is it installed?)"
 
    echo "********************************************************"
    echo "Cleanup / Copy $TEMPFILENAME to $FILENAME"
    echo "********************************************************"
 
-   rm -f "$FILENAME"
+   rm -vf "$TEMPSRTFILENAME"
+   rm -vf "$FILENAME"
    mv -f "$TEMPFILENAME" "${FILENAME%.ts}.mkv"
-   chmod 777 "$FILENAME" # This step may no tbe neccessary, but hey why not.
+   ##chmod 777 "$FILENAME" # This step may no tbe neccessary, but hey why not.
 
    echo "Done.  Congrats!"
 else
